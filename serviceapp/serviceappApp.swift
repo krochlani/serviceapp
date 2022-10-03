2616add0572244e7b11b0d42eaaa87f1c8fc22a1
Kyna Rochlani <kynarochlani@acs.sch.ae>
2022-10-03 20:27:11 +0400

    Initial Commit

---
+++ serviceapp/serviceappApp.swift
@@ -0,0 +1,17 @@
+//
+//  serviceappApp.swift
+//  serviceapp
+//
+//  Created by Kyna Rochlani on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct serviceappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
